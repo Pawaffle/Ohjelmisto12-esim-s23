c0d454b6bd76ec9b306f56d33600dea1c3da45de
Pavel Degterev <pdegeterev@gmail.com>
2023-08-22 11:31:54 +0300

    exersise 22.8.23

--- Moduli 1/classwork.py
+++ Moduli 1/classwork.py
@@ -1,3 +1,4 @@
+'''
 #tulostetaan komentin
 print('terve \n tuntematon henki!')
 
@@ -10,3 +11,14 @@ age1 = age + 1
 print('Terve,', name)
 print('Sun ikä on ' + str(age))
 print(f'vuoden päästä olet {age1} vuotta vanha')
+'''
+'''
+num1 = input('anna luku 1: ')
+num2 = input('anna luku 2: ')
+result = int(num1) / int(num2)
+
+# luku :'merkkien määrä'.'desimaali merkien määrä'f
+print(f'Jakolaskun tulos: {result:.2f}')
+'''
+import math
+print(f'Piin arvo {math.pi:.5f}')
