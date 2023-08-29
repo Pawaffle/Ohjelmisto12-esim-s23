3f9bf5a35c035f2a9387bf249c8e432d0ca57772
Pavel Degterev <pdegeterev@gmail.com>
2023-08-29 10:36:43 +0300

    10:40 29/8/23

---
+++ Mod_04/03,0_m4.py
@@ -0,0 +1,19 @@
+print('Ohjelma tulostaa saaduista luvuista pienimmän ja suurimman')
+print('Jos painat enteria ilman lukua - ohjelma lopettaa toimintansa \n')
+
+var = input('Anna joku luku: ')
+min = max = var
+
+while var != '':
+
+    if var < min:
+        min = var
+
+    if var > max:
+        max = var
+
+    var = input('Anna seurava luku: ')
+
+print(f'''
+{min} - on pienin sinun luvuista
+{max} - on isoin sinun luvuista ''')
