aa6493a21d8a9f11cf17efaae4873cf223a562dc
Pavel Degterev <pdegeterev@gmail.com>
2023-08-29 09:21:00 +0300

    9:20 29/8/23

---
+++ Mod_04/01_m4.py
@@ -0,0 +1,18 @@
+# ensimainen tapa rakenta ohjelman
+
+x = int(3)
+
+while x < 1000:
+    print(x)
+    x += 3
+
+# toinen tapa ohjelmoida
+'''
+x = 1
+
+while x < 1000:
+
+    if x % 3 == 0:
+        print(x)
+    x += 1
+'''
