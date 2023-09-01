e820aee06add7e48c85fd793389d3990c71dec9b
Pavel Degterev <pdegeterev@gmail.com>
2023-09-01 09:54:45 +0300

    9:55 1/9/23

--- Mod_05/m5_02.py
+++ Mod_05/m5_02.py
@@ -1,9 +1,13 @@
-number_list=[]
+number_list = []
 
-luku = input('Anna joku luku tai "Enter" lupuksi:' )
+luku = input('Anna joku luku tai "Enter" lupuksi: ')
 
 while luku != '':
-
+    luku = int(luku)
     number_list.append(luku)
+    luku = input('Anna joku luku tai "Enter" lupuksi: ')
 
-    luku = input('Anna joku luku tai "Enter" lupuksi:')
+print(number_list)
+number_list.sort(reverse=True)
+print((number_list))
+print(number_list[:5])
