d1a9f732a34bb7808b1608beb24aa7797eac4a3a
Pavel Degterev <pdegeterev@gmail.com>
2023-09-04 13:25:27 +0300

    13:25 4/9/23

--- Mod_03/m3_01.py
+++ Mod_03/m3_01.py
@@ -4,6 +4,6 @@ print()
 
 if length < 37:
     print('Kuha on alamittainen, kun sen pituus on alle 37 cm.')
-    print('Laske kuhan takaisin järveen!!!')
+    print(f'Pituudesta puuttu {37-length}. Laske kuhan takaisin järveen!!!')
 else:
     print('Hyvää poikka, voit pitää sen!')
