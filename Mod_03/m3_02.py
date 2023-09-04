8fd6b4937cf8290849c319fe3b69b6133e0517dd
Pavel Degterev <pdegeterev@gmail.com>
2023-09-04 13:28:22 +0300

    13:30 4/9/23

--- Mod_03/m3_02.py
+++ Mod_03/m3_02.py
@@ -6,7 +6,7 @@ elif clss == 'A' or clss =='a':
     print('A on ikkunallinen hytti autokannen yläpuolella.')
 elif clss in {'B', 'b'}:
     print('B on ikkunaton hytti autokannen yläpuolella.')
-elif clss in {'C', 'c'}:
+elif clss.upper() == 'C':
     print('C on ikkunaton hytti autokannen alapuolella.')
 else:
     print('Virheellinen hyttiluokka')
