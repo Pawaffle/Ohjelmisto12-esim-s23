79b4ca9c859bdc713341c3b0f7125db10cc8d9b7
Pavel Degterev <pdegeterev@gmail.com>
2023-09-12 10:31:42 +0300

    10:30 12/9/23

--- Mod_06/m6_05v1.py
+++ Mod_06/m6_05v1.py
@@ -1,12 +1,13 @@
 lst = []
 
-def nums(lst):
-    global even_lst
-    even_lst = []
-    for i in lst:
-        if i % 2 == 0:
-            even_lst.append(i)
-    return even_lst
+def nums(lista):
+    i = 0
+    while i < len(lista):
+        if lista[i] % 2 != 0:
+            lista.remove(lista[i])
+            i -= 1
+        i += 1
+    return lista
 
 print('Jos painat "Enter" ilman lukua, ohjelma päätyy')
 num = input('Anna joku luku: ')
@@ -17,8 +18,11 @@ while num != '':
     num = input('Anna seurava luku: ')
 
 lst.sort()
+even_lst = []
+even_lst.extend(lst)
+nums(even_lst)
 
 print(f'''
 Sinun listassa on seuravat luvut: {lst}
-Parilliset niista ovat {nums(lst)}
+Parilliset niista ovat {even_lst}
 ''')
