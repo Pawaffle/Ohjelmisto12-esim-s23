b709023a7730b48a655cb8c56314ec1f74455dc9
Pavel Degterev <pdegeterev@gmail.com>
2023-10-24 13:01:05 +0300

    13:00 24/10/23

--- Mod_09/m9_04v1.py
+++ Mod_09/m9_04v1.py
@@ -11,6 +11,8 @@ class Auto:
     speed = 0
     dist = 0
     time = 0
+    min = 0
+    sec = 0
     def __init__(self, num_plate, max_speed):
         self.reg_plate = num_plate
         self.max_speed = max_speed
@@ -27,6 +29,12 @@ class Auto:
         self.dist += (time * self.speed)
         self.time += time
 
+    def finish_time(self):
+        help_time = self.time % 1
+        self.sec = int(3600 * help_time)
+        self.min = self.sec // 60
+        self.sec -= self.min * 60
+
 
 for i in range (1,11):
     plate = random_char(3) + '-' + str(i)
@@ -38,15 +46,24 @@ while len(race) > 0:
         car.accelerate(random.randint(-10, 15))
         car.drive(1)
         if car.dist >= 10000:
+
+            over_dist = car.dist - 1000
+            over_time = over_dist / car.speed
+            car.time -= over_time
+
+            car.finish_time()
+
             finish.append(car)
             race.remove(car)
+
+
 
 finish.sort(key=lambda x: x.time, reverse=False)
 
 for car in finish:
     print(f'\nAuto, jonka rekisterinumero on {car.reg_plate}, ajoi maaliin numerolla {finish.index(car) + 1} \n'
           f'Sen huippunopeus oli {car.max_speed} km/t \n'
-          f'Auton ajettu matka {car.dist} km se on suorittu {car.time}:ssa tunnissa')
+          f'Auton kilpailu suorittu {car.time:.0f}:ssa tunnissa, {car.min}:ssa minutissa ja {car.sec}:ssa sekunnissa')
 
 '''
 car_name = 'car_' + str(i)
